5baa9f9609c51c2c2b8f560d60df2e84d768d8c5
霜暮雪 <Frost.rA9@gmail.com>
2023-04-29 20:58:00 +0800

    init : project init

---
+++ BBS-Backend/src/main/java/org/droplet/BBSBackendApplication.java
@@ -0,0 +1,13 @@
+package org.droplet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BBSBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BBSBackendApplication.class, args);
+    }
+
+}
---
+++ BBS-Backend/src/test/java/org/droplet/BBSBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.droplet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BBSBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
