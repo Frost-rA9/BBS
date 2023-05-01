7b365fd15f6185bf9326b04fc79a7f74abf5981c
霜暮雪 <Frost.rA9@gmail.com>
2023-05-01 16:18:50 +0800

    feat : from validate

--- BBS-Frontend/src/views/Welcome.vue
+++ BBS-Frontend/src/views/Welcome.vue
@@ -12,20 +12,22 @@
     <div class="welcome-title">
       <div style="font-size: 30px;font-weight: bold">欢迎来到论坛</div>
     </div>
-    <div style="width: 400px;background-color: white">
-      <transition name="el-fade-in-linear">
-        <router-view></router-view>
-      </transition>
+    <div style="width: 400px;background-color: white;z-index: 1">
+      <router-view v-slot="{Component}">
+        <transition name="el-fade-in-linear">
+          <component :is="Component"/>
+        </transition>
+      </router-view>
     </div>
   </div>
 </template>
 
 <style scoped>
 .welcome-title {
-    position: absolute;
-    bottom: 30px;
-    left: 30px;
-    color: white;
-    text-shadow: 0 0 10px black;
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
 }
 </style>
